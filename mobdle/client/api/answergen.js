6a328d3f444515d68325c22d19ab4ba0c010ccb4
adrianCiepli <adrianciepli@gmail.com>
2026-07-03 00:23:04 -0400

    Answer Generation Logic Update
    More random dailyIndex generation based on date added for those who know of mobs.js. Also added future TODO's.

--- mobdle/client/api/answergen.js
+++ mobdle/client/api/answergen.js
@@ -1,6 +1,8 @@
 import mobs from "./mobs.js";
 
-// Future logic may need to account for user local time, probably via having a useEffect() on mount in App.jsx that sends the user time zone to the server
+// TODO: Account for user's personal time so that each user from different timezone can get new answer at 12:00
+// OR: Have a countdown on the website to when there will be a new answer so that they know, and then can use UTC
+// Ensure that in whichever case, GuessArea.jsx uses time as well to check for localStorage clearing, so update accordingly there
 
 const getDailyAnswer = () => {
   const mobKeys = Object.keys(mobs);
@@ -22,8 +24,8 @@ const getDailyAnswer = () => {
   // Combines date into single num
   const dateSeed = (year * 10000) + (month * 100) + day;
 
-  // Map date to an answer's index
-  const dailyIndex = dateSeed % mobKeys.length;
+  // Slightly complicated formula to make it feel more random if you know about mobs.js, can change 1013 to a different prime at will
+  const dailyIndex = ((dateSeed * 1013) + 47) % totalMobs;
 
   const dailyMobKey = mobKeys[dailyIndex];
 
