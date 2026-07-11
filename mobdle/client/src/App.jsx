71e0abc8ee694d2d2e40d25eb1d3b7638a3c4dad
adrianCiepli <adrianciepli@gmail.com>
2026-07-11 18:00:26 -0400

    Media Queries in Individual Component CSS Files

--- mobdle/client/src/App.jsx
+++ mobdle/client/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
       <div className='title-area'>
         <p className='under-title'>The Minecraft Mob Guessing Game</p>
         {/* <h1 className="main-title">Mobdle</h1> */}
-        <img src='/title_text.png' className='main-title' style={{ width: '30%', height: '35%' }} />
+        <img src='/title_text.png' className='main-title' />
       </div>
       {/* {gotCorrect && <div className='shareable-guesses' onClick={handleCopy}><img src='/share_white.png' className='share-button' alt='Share Button' />
         <div className='shareable-copy-text'>{copied ? "Copied!" : "Share results"}</div></div>} */}
